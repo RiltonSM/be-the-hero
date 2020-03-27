61c9840157de1289d21b7e5334bbf0bee7eaf19b
Rilton Moreira <rilton_moreira@hotmail.com>
2020-03-27 00:52:50 -0300

    Mobile app

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
